e02d4810e42f8b732125a6a580bb530b46b63a5c
jammin804 <jammin804@gmail.com>
2015-06-04 06:41:24 -0400

    Pushing for work

--- js/apps.js
+++ js/apps.js
@@ -6,7 +6,29 @@ $(document).ready(function() {
 			search = $('#search').val(),
 			apiKey = '139361-BingeBuc-TUZZBGM3';
 
+        var requestURL = 'http://www.tastekid.com/api/similar?callback=?';
+
+
 		//collect data
-		results.html('Your new addiction related to  ' + search + ' are:');
+        $.getJSON(requestURL, {
+            'apikey': apiKey,
+            'q': search,
+        }, function(data){
+            if(data.results && data.results.length > 0)
+            {
+                var bingeResults = '<p>Here is what we came up with :D!</p>';
+                $.each(data.results, function(i, rep){
+                    if('movie' === rep.type.toLowerCase()){
+                        bingeResults += '<p>';
+                        bingeResults += rep.name;
+                        bingeResults += '</p>';
+                    }
+                });
+                results.html(bingeResults);
+            }else {
+                results.html('<p>Please enter a tv show or movie title.</p>')
+            }
+        });
 	});
+
 });
